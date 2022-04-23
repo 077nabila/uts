7ee90578016a5c6453c9a6019991f5896a9e6922
Nabila Regitasyari <84031548+077nabila@users.noreply.github.com>
2022-04-23 14:47:13 +0700

    Add files via upload

---
+++ from_077.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Tambah Data</title>
+</head>
+<body>
+   <h1>Form Tambah Db_077</h1> 
+   <form method="post" action="action.php">
+       <table>
+            <tr class="form-group">
+                <td>
+                    <label>Nama 077</label>
+                </td>
+                <td>
+                    <input type="text" name="nama_077" placeholder="Input Nama disini !" ruqueired>
+                </td>
+            </tr>
+            <tr class="form-group">
+                <td>
+                    <label>Email 077</label>
+                </td>
+                <td>
+                    <input type="text" name="email_077" placeholder="Input Email disini !" ruqueired>
+                </td>
+            </tr>
+            <tr>
+                    <td>
+                        <button type="submit" name="simpan">Simpan</button>
+                    </td>
+                </tr>
+       </table>
+   </form>
+</body>
+</html>
